671c844df798b602d41f8e5f5b7a6bcbd344809d
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-07-30 15:21:06 -0500

    update bootstrap for joda-time

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,3 +1,4 @@
+import org.joda.time.*
 import co.freeside.demo.*
 import static co.freeside.demo.Gender.*
 import static co.freeside.demo.Salutation.*
@@ -6,7 +7,7 @@ class BootStrap {
 
     def init = { servletContext ->
     	new Person(salutation: Mr, firstName: 'Bobby', lastName: 'Newmark', gender: male, email: 'count0@esfa.net', address: new Address(street: '3A Covina Concourse Court', city: 'Barrytown, NJ', country: 'USA')).save(failOnError: true)
-    	new Person(salutation: Mr, firstName: 'Bart', lastName: 'Simpson', gender: male, birthDate: new Date(89, 11, 17), email: 'bart@eatmyshor.ts', address: new Address(street: '57 Evergreen Terrace', city: 'Springfield', country: 'USA')).save(failOnError: true)
+    	new Person(salutation: Mr, firstName: 'Bart', lastName: 'Simpson', gender: male, birthDate: new LocalDate(1989, 12, 17), email: 'bart@eatmyshor.ts', address: new Address(street: '57 Evergreen Terrace', city: 'Springfield', country: 'USA')).save(failOnError: true)
     }
 
     def destroy = {
